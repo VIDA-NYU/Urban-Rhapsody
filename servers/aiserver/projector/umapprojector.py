b0598b972dfc1c0fd06317cf07e39d0288df0281
Joao Rulff <jlrulff@gmail.com>
2021-10-27 13:44:03 -0400

    gpu umap

--- servers/aiserver/projector/umapprojector.py
+++ servers/aiserver/projector/umapprojector.py
@@ -1,5 +1,5 @@
 import umap 
-# from cuml.manifold import UMAP
+from cuml.manifold.umap import UMAP
 # from tsnecuda import TSNE
 
 import numpy as np
@@ -11,16 +11,16 @@ class UMAPProjector:
 
         params = UMAPProjector.__parse_params( params )
 
-        # embeddings = np.array(embeddings)
+        embeddings = np.array(embeddings, dtype=np.float32)
         # umap_projection = TSNE(n_components=2, perplexity=15, learning_rate=10).fit_transform(embeddings)
-        # umap_projection = UMAP().fit_transform(embeddings)
+        umap_projection = UMAP().fit_transform(embeddings)
 
-        umap_projection = umap.UMAP(
-            random_state=42, 
-            n_neighbors=params['n_neighbors'],
-            min_dist=params['min_dist'],
-            metric=params['metric'],
-            n_jobs=5).fit_transform(embeddings)
+        # umap_projection = umap.UMAP(
+        #     random_state=42, 
+        #     n_neighbors=params['n_neighbors'],
+        #     min_dist=params['min_dist'],
+        #     metric=params['metric'],
+        #     n_jobs=5).fit_transform(embeddings)
 
         return umap_projection[:,0], umap_projection[:,1]
 
