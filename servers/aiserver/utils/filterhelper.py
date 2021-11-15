b37051325a801cc4f6fd23545b9146c60c85a7a9
Joao Rulff <jlrulff@gmail.com>
2021-11-15 18:11:42 -0500

    merging server

--- servers/aiserver/utils/filterhelper.py
+++ servers/aiserver/utils/filterhelper.py
@@ -7,16 +7,17 @@ class FilterHelper:
 
         for day in indexNeighbors:
             
-            for frameuid in indexNeighbors[day]['frames']:
+            for index, frameuid in enumerate(indexNeighbors[day]['frames']):
                 
                 if( frameConfidence[frameuid] >= confidenceThreshold ):
 
                     if( not( day in neighborsPerDay ) ):
                         
                         ## creating new day entry
-                        neighborsPerDay[day] = { 'count': 0, 'frames': [] }
+                        neighborsPerDay[day] = { 'count': 0, 'frames': [], 'paths': [] }
 
                     neighborsPerDay[day]['frames'].append( frameuid )
+                    neighborsPerDay[day]['paths'].append( indexNeighbors[day]['paths'][index] )
                     neighborsPerDay[day]['count'] += 1
                 
                 
